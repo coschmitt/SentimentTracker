157a61865aeb615fd43bf15f2965d9753f75f989
jcwatson2 <jcwatson2@wisc.edu>
2021-04-17 11:59:09 -0500

    refactored html

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class user(models.Model):
+    user_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
--- tweets/urls.py
+++ tweets/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path("sign-up", views.SignUp.as_view(), name="sign-up"),
     path("sign-in", views.SignIn.as_view(), name="sign-in"),
     path("auth_home", views.home, name="auth_home"),
-    path("signout", views.SignOut.as_view(), name='sign-out'),
-
+    path("signout", views.SignOut.as_view(), name='sign-out')
 ]

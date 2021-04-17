ca5b13e799cd986f4599c59562e92f646e54ba62
coschmitt <charlieschmitt123@gmail.com>
2021-04-17 17:21:26 -0500

    refactored the region input

--- tweets/forms.py
+++ tweets/forms.py
@@ -4,14 +4,13 @@ from django import forms
 class SearchForm(forms.Form):
     REGIONS = (
         ("All regions", "All regions"),
-        ('Southwest', 'Southwest'),
-        ('Westcoast', 'Westcoast'),
-        ('Eastcoast', 'Eastcoast'),
-        ('Midwest', 'Midwest'),
-        ('South', 'South'),
-        ('Northeast', 'Northeast'),
-        ('Pacific Northwest', 'Pacific Northwest'),
-        ('Rocky Mountains', 'Rocky Mountains')
+        ('Southwest', '34.694910,-105.981320,450'),
+        ('Westcoast', '37.696473,-117.139650,450'),
+        ('Northeast', '41.474976,-76.508037,450'),
+        ('Midwest', "41.782133,-87.999337,450"),
+        ('South', '34.560107,-85.692607,450'),
+        ('Pacific Northwest', '45.582650,-119.882060,450'),
+        ('Rocky Mountains', '41.672229,-108.280497,450')
     )
 
     TWEET_TYPES = (
@@ -26,4 +25,5 @@ class SearchForm(forms.Form):
         input_formats=['%Y/%m/%d'], label="End date (default today): ", required=False
     )
     result_type = forms.ChoiceField(choices=TWEET_TYPES)
+
 

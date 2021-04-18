967784e0a8d39b5a7876051d8f1d6e6d4bf90864
coschmitt <charlieschmitt123@gmail.com>
2021-04-18 11:45:00 -0500

    Added env file and refactored code

--- tweets/views.py
+++ tweets/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import render
 from django.views.generic import FormView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-import matplotlib.pyplot as plt
 import numpy as np
 from datetime import date
 
@@ -15,17 +14,9 @@ from tweets.forms import SearchForm, TrendsFilterForm
 from tweets.graphAnalysis.woied_helpers import get_trending, get_woeid
 from tweets.forms import SearchForm
 import datetime as datetime
-import tweepy
-import boto3
-import matplotlib.pyplot as plt, mpld3
-import pandas as pd
+
 from datetime import datetime,timedelta
-import requests
-import json
-
-
-geocode_key = 'AIzaSyAiou_UIwyXs16YbrfMzj1j58mQ-NPExjk'
-
+from tweets.graphAnalysis.graphSentiment import generate_graph
 
 
 
@@ -112,7 +103,6 @@ class Search(FormView):
         region = form.cleaned_data['region']
         end_date = date.today()
         result_type = form.cleaned_data['result_type']
-        print(search, region, end_date, result_type)
         return HttpResponseRedirect(self.get_success_url() + str(search) + '/'+ str(region) + '/'
                                     + str(end_date) + '/' + str(result_type) + '/')
 
@@ -131,8 +121,9 @@ class FilterTrends(FormView):
         return HttpResponseRedirect(self.get_success_url() + str(location)+'/'+str(date)+'/')
 
 
+'''Displays graph based on search query.'''
 def display(request, search=None, region=None, end_date=None, result_type=None):
-    graphs = generate_graph(search, region, end_date, result_type)
+    graphs = generate_graph(search, region, end_date, result_type)  # helper function used for making graphs
 
     context = {'graph_bar': graphs[0],
                'graph_line': graphs[1],
@@ -144,10 +135,11 @@ def display(request, search=None, region=None, end_date=None, result_type=None):
 
 
 def display_trends(request, woeid=1, date=None):
-    trends = get_trending(woeid=woeid)[0].get('trends')
-    page = request.GET.get('page', 1)
+    trends = get_trending(woeid=woeid)[0].get('trends') # display trends based on a certain location, or default to 1
+    page = request.GET.get('page', 1)                   # (worldwide trends)
     paginator = Paginator(trends, 10)
 
+    # Pagination logic
     try:
         trends = paginator.page(page)
     except PageNotAnInteger:
@@ -163,92 +155,74 @@ def return_graph(search, region, end_date, result_type):
     y = np.sin(x)
 
 
-def generate_graph(search, region, end_date, result_type):
-    print(search, region, end_date, result_type)
-    comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
-
-    auth = tweepy.OAuthHandler("KdE45VAZRJYDcjaqHz1NYuRSb", "IqeYxJgRHr4FDdz5lktEYcNtQsQwvELMWbGIb2EAjyVQXEDgoz")
-    auth.set_access_token("1252352172085383168-Rz2h4E6riMibKxFeS4xfzGgvNHy0TL", "aCLAFYRNgq6zZ0laJS9Q2gbkKa1x2VPlJrvzVCe4dQ9zT")
-
-    api = tweepy.API(auth)
-    # user = api.get_user('joebiden')
-
-
-    api = tweepy.API(auth)
-    numTweets = 5
-    numDays = 7
-    end_date = datetime.strptime(end_date, '%Y-%m-%d')
-    dataf = [] #For storing dataframe of valuable information
-    for i in range(0,numDays):
-        untilDate = datetime.strptime(datetime.now().strftime('%Y-%m-%d'),'%Y-%m-%d')
-        print(type(end_date))
-
-        print(end_date)
-        print(type(end_date))
-        keyword_tweets = api.search(q=search, rpp=100, count=numTweets, until = end_date-timedelta(days = i))
-
-        #gathering info about timeline
-        text = "" #String to store users tweets
-        day  = "" #String to check what day of the week this tweet was posted
-
-        print(len(keyword_tweets))
-        for status in keyword_tweets:
-            day = status._json["created_at"][0:3]
-            text = status._json["text"]
-            data = json.loads(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
-            overallSentiment = data["Sentiment"]
-            positive = data["SentimentScore"]["Positive"]
-            negative = data["SentimentScore"]["Negative"]
-            dataf.append([day, positive, negative])
-            print(status)
-
-
-    dataf.reverse()
-    print(dataf)
-
-
-    df = pd.DataFrame(dataf, columns=['Day', 'positive', 'negative%'])
-
-    df["positive"] = 100 * df["positive"]
-    df["negative%"] = 100 * df["negative%"]
-
-    dfDay = df.groupby(['Day']).mean()
-
-    dfDay = dfDay.reindex(["Mon", "Tue", "Wed", "Thu", "Fri","Sat","Sun"])
-
-    dfDay=dfDay.fillna(0)
-    print(dfDay)
-    dayList = dfDay.index.get_level_values('Day')
-
-    positiveList = dfDay.reset_index()["positive"].tolist()
-    negativeList = dfDay.reset_index()["negative%"].tolist()
-    print(dayList)
-    # Bar graph
-
-    fig1 = plt.figure()
-    plt.bar(dayList, negativeList)
-    # naming the y axis
-    plt.ylabel('Percent')
-
-    plt.title(search + ' Sentiment Analysis!')
-
-    fig1 = mpld3.fig_to_html(fig1)
-    # plt.plot(dayList, positiveList)
-    # barGraph = plt.show()
-
-    #Line graph
-    # Line graph for more days
-
-
-
-
-    fig2 = plt.figure()
-    plt.plot(dayList, positiveList)
-    plt.xlabel('Day of the Week')
-    # naming the y axis
-    plt.ylabel('Percent positivity')
-    plt.title(search + ' Sentiment Analysis!')
-    fig2 = mpld3.fig_to_html(fig2)
-
-    return [fig1,fig2]
+# def generate_graph(search, region, end_date, result_type):
+#     comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
+#
+#     auth = tweepy.OAuthHandler("KdE45VAZRJYDcjaqHz1NYuRSb", "IqeYxJgRHr4FDdz5lktEYcNtQsQwvELMWbGIb2EAjyVQXEDgoz")
+#     auth.set_access_token("1252352172085383168-Rz2h4E6riMibKxFeS4xfzGgvNHy0TL", "aCLAFYRNgq6zZ0laJS9Q2gbkKa1x2VPlJrvzVCe4dQ9zT")
+#
+#     api = tweepy.API(auth)
+#     numTweets = 5
+#     numDays = 7
+#     end_date = datetime.strptime(end_date, '%Y-%m-%d')
+#     dataf = []  # For storing dataframe of valuable information
+#     for i in range(0,numDays):
+#         untilDate = datetime.strptime(datetime.now().strftime('%Y-%m-%d'),'%Y-%m-%d')
+#         keyword_tweets = api.search(q=search, rpp=100, count=numTweets, until=end_date-timedelta(days=i),
+#                                     result_type=result_type, region=region)
+#
+#         # gathering info about timeline
+#         # text = "" #String to store users tweets
+#         # day  = "" #String to check what day of the week this tweet was posted
+#
+#         for status in keyword_tweets:
+#             day = status._json["created_at"][0:3]
+#             text = status._json["text"]
+#             data = json.loads(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+#             overallSentiment = data["Sentiment"]
+#             positive = data["SentimentScore"]["Positive"]
+#             negative = data["SentimentScore"]["Negative"]
+#             dataf.append([day, positive, negative])
+#
+#
+#     dataf.reverse()
+#
+#     df = pd.DataFrame(dataf, columns=['Day', 'positive', 'negative%'])
+#
+#     df["positive"] = 100 * df["positive"]
+#     df["negative%"] = 100 * df["negative%"]
+#
+#     dfDay = df.groupby(['Day']).mean()
+#
+#     dfDay = dfDay.reindex(["Mon", "Tue", "Wed", "Thu", "Fri","Sat","Sun"])
+#
+#     dfDay=dfDay.fillna(0)
+#     dayList = dfDay.index.get_level_values('Day')
+#
+#     positiveList = dfDay.reset_index()["positive"].tolist()
+#     negativeList = dfDay.reset_index()["negative%"].tolist()
+#
+#     # Bar graph
+#
+#     fig1 = plt.figure()
+#     plt.bar(dayList, negativeList)
+#     # naming the y axis
+#     plt.ylabel('Percent')
+#
+#     plt.title(search + ' Sentiment Analysis!')
+#
+#     fig1 = mpld3.fig_to_html(fig1)
+#
+#     #Line graph
+#     # Line graph for more days
+#
+#     fig2 = plt.figure()
+#     plt.plot(dayList, positiveList)
+#     plt.xlabel('Day of the Week')
+#     # naming the y axis
+#     plt.ylabel('Percent positivity')
+#     plt.title(search + ' Sentiment Analysis!')
+#     fig2 = mpld3.fig_to_html(fig2)
+#
+#     return [fig1,fig2]
 
